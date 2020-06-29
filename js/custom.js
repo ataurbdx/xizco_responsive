2598a875735cc72c52cbe7783aca57477036ae90
ataurwub <ataurrahman.wub@gmail.com>
2020-06-29 15:31:53 +0600

    color picker added

--- js/custom.js
+++ js/custom.js
@@ -39,6 +39,8 @@ $(function () {
         autoplaySpeed: 2000,
         dots: true,
     });
+    
+    
 
     $(".review_slide").slick({
         arrows: false,
@@ -49,18 +51,27 @@ $(function () {
         autoplaySpeed: 2000,
         dots: true,
     });
+    
+    
+    
 
     $('.faq_header').on('click', function () {
         $('.faq_header button').removeClass('active');
         $(this).children('button').addClass('active');
     });
 
+    
+    
+    
     var mixer = mixitup('.mixitup_wrap');
 
     $('.work_item').venobox({
         share: ['']
     });
 
+    
+    
+    
     $('.counter').counterUp({
         delay: 10,
         time: 1000
@@ -76,7 +87,7 @@ $(function () {
 
     // Simple example, see optional options for more configuration.
     const pickr = Pickr.create({
-        el: '.color_picker',
+        el: '.color_picker_theme',
         theme: 'classic', // or 'monolith', or 'nano'
 
         swatches: [
@@ -107,9 +118,6 @@ $(function () {
             interaction: {
                 hex: true,
                 rgba: true,
-                hsla: true,
-                hsva: true,
-                cmyk: true,
                 input: true,
                 clear: true,
                 save: true
@@ -122,6 +130,116 @@ $(function () {
         var colorCode = color.toHEXA().toString();
         
         document.documentElement.style.setProperty("--themeColor", colorCode)
+    console.log(color.toHEXA().toString());
+//    console.log(color.toRGBA().toString());
+})
+    
+    
+    
+    
+    
+    
+    
+    //    COLOR PICKER JS
+
+    // Simple example, see optional options for more configuration.
+    const pickrr = Pickr.create({
+        el: '.color_picker_btn',
+        theme: 'classic', // or 'monolith', or 'nano'
+
+        swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+        components: {
+
+            // Main components
+            preview: true,
+            opacity: true,
+            hue: true,
+
+            // Input / output Options
+            interaction: {
+                hex: true,
+                rgba: true,
+                input: true,
+                clear: true,
+                save: true
+            }
+        }
+    });
+    
+    
+    pickrr.on('change', (color, instance) => {
+        var colorCode = color.toHEXA().toString();
+        
+        document.documentElement.style.setProperty("--btnColor", colorCode)
+    console.log(color.toHEXA().toString());
+//    console.log(color.toRGBA().toString());
+})
+    
+    
+    
+    
+    //    COLOR PICKER JS
+
+    // Simple example, see optional options for more configuration.
+    const pickrrr = Pickr.create({
+        el: '.color_picker_overlay',
+        theme: 'classic', // or 'monolith', or 'nano'
+
+        swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+        components: {
+
+            // Main components
+            preview: true,
+            opacity: true,
+            hue: true,
+
+            // Input / output Options
+            interaction: {
+                rgba: true,
+                input: true,
+                clear: true,
+                save: true
+            }
+        }
+    });
+    
+    
+    pickrrr.on('change', (color, instance) => {
+        var colorCode = color.toHEXA().toString();
+        
+        document.documentElement.style.setProperty("--overlayColor", colorCode)
     console.log(color.toHEXA().toString());
 //    console.log(color.toRGBA().toString());
 })
